d744bd6e722a1a99e2990be67fc0fe417fb40c78
davidcraig <davidcraig1986@gmail.com>
2025-11-13 16:57:37 +0000

    Add debt schedule and adjust style.

--- src/components/DebtSchedule.tsx
+++ src/components/DebtSchedule.tsx
@@ -1,36 +1,74 @@
 import useDebts from "../stores/debts";
 import ucFirst from "../functions/ucFirst";
 
+const nextMonth = (date: Date) => {
+  const next = new Date(date);
+  next.setMonth(next.getMonth() + 1);
+  return next;
+};
+
+let month;
+
+const renderMonthName = (date: Date) => {
+  const options = { month: "long", year: "numeric" };
+  return date.toLocaleDateString(undefined, options);
+};
+
 function DebtSchedule() {
   const { debts } = useDebts();
+  const now = new Date();
+
+  const mappedDebts = debts.map((debt) => {
+    return {
+      name: debt.name,
+      balance: debt.balance,
+      monthlyMinimumPayment: debt.monthlyMinimumPayment,
+      type: ucFirst(debt.type),
+      months: Math.ceil(debt.balance / debt.monthlyMinimumPayment),
+      newBalance: debt.balance,
+    };
+  });
+
+  const maxMonths = mappedDebts.reduce((best, d) => {
+    return d.months > best ? d.months : best;
+  }, 0);
+
+  const rows = [];
+
+  month = nextMonth(now);
+  for (let i = 0; i < maxMonths; i++) {
+    rows.push(
+      <tr key={i}>
+        <td>{renderMonthName(month)}</td>
+        {mappedDebts.map((debt) => {
+          return (
+            <td>
+              {debt.newBalance > 0
+                ? debt.newBalance > debt.monthlyMinimumPayment
+                  ? (debt.newBalance -= debt.monthlyMinimumPayment)
+                  : (debt.newBalance = 0)
+                : ""}
+            </td>
+          );
+        })}
+      </tr>,
+    );
+    month = nextMonth(month);
+  }
 
   return (
     <>
-      {!debts ||
-        (debts.length === 0 && (
-          <div>No debts found, use the form on the left to add some.</div>
-        ))}
-
-      {debts && debts.length > 0 && (
-        <table className="table-auto w-full">
+      {mappedDebts && mappedDebts.length > 0 && (
+        <table className="table-auto md:table-fixed w-full">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Amount</th>
-              <th>Minimum Payment</th>
-              <th>Type</th>
+              <th>Date</th>
+              {mappedDebts.map((debt) => (
+                <th key={debt.name}>{debt.name}</th>
+              ))}
             </tr>
           </thead>
-          <tbody>
-            {debts.map((debt) => (
-              <tr key={debt.id}>
-                <td>{debt.name}</td>
-                <td>{debt.balance}</td>
-                <td>{debt.monthlyMinimumPayment}</td>
-                <td>{ucFirst(debt.type)}</td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{rows.map((r) => r)}</tbody>
         </table>
       )}
     </>
